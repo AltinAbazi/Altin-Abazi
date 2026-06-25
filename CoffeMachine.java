8159751ef5cadea5fb2a0543e74efc62c5c35c91
AltinAbazi <142097899+AltinAbazi@users.noreply.github.com>
2026-06-25 17:46:22 +0200

    fourth save

--- CoffeMachine.java
+++ CoffeMachine.java
@@ -8,7 +8,9 @@ public class CoffeMachine {
        
         System.out.println("HELLO \nWelcome to our coffe machine.\nPlease instert your coins!");
        System.out.println("How much money do you want to insert???\n1.= 0.50 $\n2.= 1 $\n3.= 2 $ \n4.= stop putting coins");
-        int coin= sc.nextInt();
+       
+        do {
+       int coin= sc.nextInt();
        switch (coin) {
                 case 1:
                 balance +=0.50;
@@ -25,9 +27,12 @@ public class CoffeMachine {
                 case 4:
                 break;            
             }
-    
-     System.out.println("Current balance: " + balance + "$");
-    
+
+     if (coin != 4) {
+                System.out.printf("Current balance:"+ balance);
+            }
+                 }while (balance != 4);
+        
     
     Scanner sc1= new Scanner(System.in);
         System.out.println("\nSelect coffee:\n1 = Americano -1.0$\n2 = Espresso-1.50$\n3 = Machiato 2.00$ ");
@@ -71,18 +76,10 @@ public class CoffeMachine {
                 System.out.println("Inserted money: "+balance);
                 System.out.println("Coffee: "+coffename);
                 System.out.println("Coffee price: "+price);
-                System.out.println("Sugar: "+sugar+"sugars");
+                System.out.println("Sugar: "+sugar+" sugars");
                 System.out.println("Thank you,\n\n\nEnjoy your coffe!!!");
           
             }
          
             }
         
-
-
-
-
-
-
-
-
