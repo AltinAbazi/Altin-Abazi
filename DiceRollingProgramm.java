56de8a30e4896d9e74fd23dac14f1f5bf3e1671a
AltinAbazi <142097899+AltinAbazi@users.noreply.github.com>
2026-07-01 11:23:59 +0200

    Remove comment from main method in DiceRollingProgramm

--- DiceRollingProgramm.java
+++ DiceRollingProgramm.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 public class DiceRollingProgramm{
-    public static void main(String[] args) {//hello
+    public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         Random random =new Random();
         int numOfDice;
